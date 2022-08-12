f25f74d57561348ca3a03cfa7f1eb22cc6f82c6f
Michael Hodgkins <michaelhodgkins@Michaels-MacBook-Pro.local>
2022-08-12 15:41:04 +0100

    all done

--- routes/drones.js
+++ routes/drones.js
@@ -31,16 +31,41 @@ router.post('/drones/create', (req, res, next) => {
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
+  const { id } = req.params;
+  Drone.findById(id)
+  .then((droneToEdit) => {
+    res.render('drones/update-form', droneToEdit);
+  })
+  .catch((err) => `Couldn/t retrieve drone when trying to edit: ${err}`);
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
+
+  const { id } = req.params;
+  const { name, propellers, maxSpeed } = req.body;
+
+  Drone.findByIdAndUpdate(
+    id,
+    { name, propellers, maxSpeed},
+    { new: true }
+  )
+   .then(() => { res.redirect(`/drones`)})
+
+   .catch((err) => console.log(`Error updating drone: ${err}`)
+   );
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
   // Iteration #5: Delete the drone
   // ... your code here
+  const { id } = req.params;
+
+  Drone.findByIdAndDelete(id)
+  .then(() => res.redirect('/'))
+  .catch((err) => console.log(`Error when deleting drone: ${err}`));
+
 });
 
 module.exports = router;
